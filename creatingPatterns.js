b3f194fe95e0e9c69549edebc60e46af7dc21b71
Alliyan <alliyan732@gmail.com>
2024-01-23 11:32:12 +0500

    patterns code  added

---
+++ creatingPatterns.js
@@ -0,0 +1,57 @@
+/*
+
+##########
+##      ##
+# #    # #
+#  #  #  #
+#   ##   #
+#   ##   #
+#  #  #  #
+# #    # #
+##      ##  
+##########       
+
+*/
+
+
+
+// createing Right triangle triangle
+/*
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+ 
+*/
+
+// for (let i = 0; i < 5; i++) {
+//     let row = ''
+//     for (let j = 5; j >= 0; j--) {
+//         if(i >= j){
+//             row += '* '
+//         }
+//     }
+//     console.log(row)
+    
+// }
+
+// createing Right triangle triangle
+/*
+
+*
+* *
+* * *
+* * * *
+* * * * *
+ 
+*/
+
+// for (let i = 0; i < 5; i++) {   // rows
+//     let row = ''
+//     for (let j = 0; j <= i; j++) {  // cols
+//         row += '* '
+//     }
+
+//     console.log(row)
+// }
