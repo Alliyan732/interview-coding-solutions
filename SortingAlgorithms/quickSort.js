cccc4d6d1bc0cba7b0ba1326062523c1612c7fef
Alliyan <alliyan732@gmail.com>
2024-01-31 06:52:09 +0500

    linked lists added

---
+++ SortingAlgorithms/quickSort.js
@@ -0,0 +1,5 @@
+// Keywords: Pivot, Partitions
+// we can take pivot: Random, First element, Median, Last Element
+// This code is implemented using last element as pivot
+// Divide and conquer rule
+// variables: low, high and i, we will initialize i with -1 beacaue it makes position for the comming element
---
+++ Trees/binarySearchTree.py
@@ -0,0 +1,11 @@
+"""
+BST Use:
+Can be used as internal implementation of Set(), we cannot insert duplicate in BST
+useful for searching because everytime we search , it reduce the searching space by half
+
+It has atmost two child nodes
+
+searching complexity = O(logn)
+insertion complexity = O(logn)
+
+"""
